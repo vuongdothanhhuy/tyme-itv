c2f3bd606b2dbd404d3cb0b6dbcd751f45947de0
Huy, Vuong Do Thanh <vuongdothanhhuy@gmail.com>
2023-11-09 18:34:01 +0700

    Update README.md

--- README.md
+++ README.md
@@ -1,46 +1,32 @@
-# Getting Started with Create React App
+# Tyme Assignment Interview
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+### What have you done so far?
 
-## Available Scripts
+- Create a simple ReactJS SPA.
+- Fetch sample data from a mocked API online free service.
+- On first load, fetch and display data.
+- Set a timer to reload new data every 60 seconds.
+- Ability to search (search string in item Title and item Description only)
+- Ability to filter (only implemented filter on `stock` property of the item)
+- Focus entirely on the body part of the page to showcase JS skill. Ignored the Header and Footer and unnecessary styling.
+- The page is responsive basically with mobile and `md` desktop breakpoints. **Tailwindcss** is used.
+- `See more` feature to load more data when clicked. Each time it loads eight items.
+- Used `flex` for general layout and `grid` for the "items" listing.
+- Used `lodash` just for quick-kill.
+- Use internal state to keep data.
 
-In the project directory, you can run:
+### What are some improvement/ missing pieces you would like to do if you have more time?
 
-### `npm start`
+- Finish the Header
+- Finish the Footer
+- Separate into smaller components instead of everything inside one big component. I sacrificed this for speed to finish the test.
+- Add some Jest test
+- Proper styling
+- Clean up code
+- Use some proper external state management system, especially when divided into multiple small components
+- Divide utils code into separated .ts module and import when needed
+- Some `lodash` functions can be rewritten to just vanillaJS to reduce dependencies.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+### How many hours you actually spent finishing the assignment?
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+- About `5 hours` continuously, but span across 2 days because of other multiple workload aside.
--- src/App.tsx
+++ src/App.tsx
@@ -86,11 +86,11 @@ function App() {
                 break
             }
             case 'low': {
-                    const result = map(data, (i) => {
-                        i.hide = i.stock > 40;
-                        return i
-                    })
-                    setData(result)
+                const result = map(data, (i) => {
+                    i.hide = i.stock > 40;
+                    return i
+                })
+                setData(result)
                 break
             }
             case 'high': {
@@ -154,12 +154,16 @@ function App() {
                     </div>
                     <div className="w-full p-2">
                         <div className="relative h-10 w-full min-w-[200px]">
-                            <select onChange={(e) => { filterItems(e.target.value)}} className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-red-500 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50">
+                            <select onChange={(e) => {
+                                filterItems(e.target.value)
+                            }}
+                                    className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-red-500 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50">
                                 <option value="all" selected>All</option>
                                 <option value="low">low stock</option>
                                 <option value="high">high stock</option>
                             </select>
-                            <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                            <label
+                                className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                                 Stock filter
                             </label>
                         </div>
@@ -169,13 +173,14 @@ function App() {
                     <div className="grid grid-cols-4 gap-4">
                         {data.length && data.map((item: any) => {
                             if (!item.hide) {
-                                return <div className={`border rounded-xl ${item.stock > 40 ? 'bg-green-400' : 'bg-red-400'} p-4`}>
+                                return <div
+                                    className={`border rounded-xl ${item.stock > 40 ? 'bg-green-400' : 'bg-red-400'} p-4`}>
                                     <figure>
                                         <img src={item.thumbnail} alt={item.title} className="h-[100px] object-fill"/>
                                     </figure>
                                     <p className="font-bold">{item.title}</p>
                                     <p>{item.brand}</p>
-                                    <p className="text-xs">{item.description.substring(0,100)}...</p>
+                                    <p className="text-xs">{item.description.substring(0, 100)}...</p>
                                 </div>
                             }
                         })}
